8f23e1b12d2097dd81e5b8294e3b67b70b51c66d
DustySource <72270606+DustySource@users.noreply.github.com>
2022-01-24 15:52:34 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use std::io;
 use rand::Rng;
-use std::cmp::Ordering;
+
 
 fn main() {
     println!("Henlo Fren!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {}", secret_number);
+    let mut secret_number = rand::thread_rng().gen_range(1..=100);
+   
 
 
  
     loop{
-
+        
+         // println!("The secret number is: {}", secret_number);
         println!("Please Guess a Number!");
         let mut guess = String::new();
         
-       
+        
     
         io::stdin()
         .read_line(&mut guess)
@@ -30,12 +31,13 @@ fn main() {
      
 
     match guess.cmp(&secret_number){
-        std::cmp::Ordering::Less => println!("Too Small! shrimp x3"),
+        std::cmp::Ordering::Less => println!("Too Small!! :O"),
         std::cmp::Ordering::Equal => {
-            println!("Nyaa!");
-            break;
+            println!("You Guessed It!!!");
+            secret_number = rand::thread_rng().gen_range(1..=100);
+         
         }
-        std::cmp::Ordering::Greater=> println!("Its TOO BIG NYAAAAA~ D:"),
+        std::cmp::Ordering::Greater=> println!("TOO BIG D:"),
         
 
     }
